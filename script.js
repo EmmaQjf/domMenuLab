3385fe117a2c8a33dfd8abdecdac81180f180cac
junfengqiao <jfqemma@gmail.com>
2026-01-13 20:29:41 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Menu data structure
+const menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+const mainEl = document.querySelector('main')
+mainEl.style.backgroundColor ='var(--main-bg)'
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>"
+mainEl.classList.add('flex-ctr')
+
+
+const topMenuEl = document.getElementById('top-menu')
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add('flex-around')
+
+for (let link of menuLinks){
+    const a = document.createElement("a");
+    a.setAttribute('href',link.href)
+    a.textContent = link.text;
+    topMenuEl.append(a)
+}
